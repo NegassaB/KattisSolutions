390163283bf6f21106a3651dd79d230294730137
Negassa Berhanu <negassab16@gmail.com>
2018-10-12 16:17:22 +0300

    The Take-Two-Stones solution
    Solution for the Take-Two-Stones problem. Incidentally have no instance objects didn't make any impact on the time complexity needed to run the solution. I was really baffled by that. I don't know if there has been any impact on the space complexity though.

---
+++ Stones.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Stones {
+	public static void main(String[] args) {
+		// int input = new Scanner(System.in).nextInt();
+		System.out.println(new Scanner(System.in).nextInt() % 2 != 0? "Alice" : "Bob");
+	}
+}
