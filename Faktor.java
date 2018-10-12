36a66cb37742d2b31424315cbca7800d877257bc
Negassa Berhanu <negassab16@gmail.com>
2018-10-12 11:58:05 +0300

    Update Faktor.java

--- Faktor.java
+++ Faktor.java
@@ -1,15 +1,3 @@
-/* import java.util.Scanner;
-public class Faktor {
-	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
-		System.out.println(Faktor.impFac(new Scanner(System.in).nextInt(), new Scanner(System.in).nextInt()));
-	}
-	
-	static int impFac(int input1, int input2) {
-		return input1 * (input2 - 1) + 1;
-	}
-}
- */
 import java.util.Scanner;
 public class Faktor {
 	public static void main(String[] args) {
