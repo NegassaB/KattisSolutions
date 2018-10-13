816bf5f4b6bf178459b226fc35ac2c0e23c3e68d
Negassa Berhanu <negassab16@gmail.com>
2018-10-13 12:42:11 +0300

    Solving for carrots solution
    this one is really confusing. Way to simple.

---
+++ Solving_For_Carrots.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Solving_For_Carrots {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int p = sc.nextInt();
+		System.out.println(p);
+	}
+}
